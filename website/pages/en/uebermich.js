9716a1c59785e8e3d245586c0756f24200821bd3
Tom Packebusch <tom.packe@gmx.de>
2022-03-10 21:33:43 +0100

    Luise changed stuff

--- website/pages/en/uebermich.js
+++ website/pages/en/uebermich.js
@@ -10,9 +10,8 @@
  //  CONTENT ===> CHANGE ONLY HERE, LUISE!
 
 const ABOUT = `
-Nein, ich wollte nicht schon immer Schriftstellerin werden. Meeresbiologin, Frontfrau einer Indierock-Band, Naturschützerin. Die Idee für den Roman kam mir im Halbschlaf, und wer würde das Buch denn schreiben, wenn nicht ich selbst? Ich schob das Vorhaben vor mir her: Erstmal Master machen, dann eine Anstellung finden. Nach zwei Jahren erfolglosem Bewerben hatte ich genug. 
-Ich suchte mir einen Teilzeitjob, der mich geistig und körperlich nicht erschöpfte und mir genug Zeit fürs Schreiben ließ. Ich schrieb auf Reisen, in Cafés, auf meinem Balkon, am See. 
-Drei Jahre, in denen ich so sehr Gefallen am kreativen Schreiben fand, dass ich nun am nächsten Manuskript arbeite. Ausgleich finde ich in meinem Beruf als Kickboxtrainerin für FLINTA*-Personen und Jugendliche im besten Kampfsportgym Leipzigs.
+Nein, ich wollte nicht schon immer Schriftstellerin werden. Meeresbiologin, Frontfrau einer Indierock-Band, Naturschützerin. Die Idee für den Roman kam mir im Halbschlaf, und wer würde das Buch denn schreiben, wenn nicht ich selbst? Zunächst schob ich das Vorhaben vor mir her: Erstmal Master machen, dann eine Anstellung finden. Nach zwei Jahren erfolglosem Bewerben hatte ich genug. Ich suchte mir einen Teilzeitjob in der Gastronomie, der mich geistig und körperlich nicht erschöpfte und mir genug Zeit fürs Schreiben ließ. Ich schrieb auf Reisen, in Cafés, auf meinem Balkon, am See. Drei Jahre arbeitete ich an "binär" und in dieser Zeit fand ich so sehr Gefallen am kreativen Schreiben, dass ich nun am nächsten Manuskript arbeite (Arbeitstitel und Zusammenfassung: Fick dich, Patriarchat!).
+Mittlerweile verkaufe ich keinen Kaffee und Kuchen mehr, sondern habe meine zweite Leidenschaft zum Beruf gemacht. Ich bin Kickboxtrainerin für FLINTA*-Personen und Jugendliche im besten Kampfsportgym Leipzigs.
 `;
 
 const BUTTON = `
